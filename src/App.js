60a6aecb76b24c6cce8cf7aac9b830f13fbd5fdb
Npmauney <nathanielmauney@gmail.com>
2023-08-12 14:13:13 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Example from './example';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className='colorThang'>Hello World</h1>
+      <Example />
     </div>
   );
 }
